0399ba7072f95e9b287ae045c47114363c4d98a2
leonardoForaneo <leonardomartinez.contact@gmail.com>
2021-07-07 18:45:20 -0500

    Migraciones

--- database/migrations/2021_07_07_225527_create_cursos_table.php
+++ database/migrations/2021_07_07_225527_create_cursos_table.php
@@ -15,6 +15,8 @@ class CreateCursosTable extends Migration
     {
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
             $table->timestamps();
         });
     }
